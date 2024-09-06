6e98f7372ede623ae011c92b59940c24a6b65e32
maulidaprina <mldaprna@gmail.com>
2024-09-06 12:36:01 +0700

    MaulidaPrina12

---
+++ MyFirstJavaClass12.java
@@ -0,0 +1,25 @@
+public class MyFirstJavaClass12 {
+    public static void main(String[] args) {
+        String nama = "Maulida Aprina Putri";
+        String nim = "244107060098";
+        String kelas = "SIB - 1C";
+        String absen = "12";
+        String jurusan = "Teknologi Informasi";
+        String prodi = "Sistem Informasi Bisnis";
+        String alamat = "Jl. Manjuhan Perum griya Sejahtera, No. 4-5";
+        String nohp = "082239442005";
+        String hobi = "Menonton Anime";
+      
+        System.out.println("+-----------------------------------------------+");
+        System.out.println("| Nama    \t: " + nama + "\t\t|");
+        System.out.println("| NIM     \t: " + nim + "\t\t\t|");
+        System.out.println("| Kelas   \t: " + kelas + "\t\t\t|");
+        System.out.println("| Absen   \t: " + absen + "\t\t\t\t|");
+        System.out.println("| Jurusan \t: " + jurusan + "\t\t|");
+        System.out.println("| Prodi   \t: " + prodi + "\t|");
+        System.out.println("| Alamat  \t: " + alamat + "\t|");
+        System.out.println("| NoHP    \t: " + nohp + "\t\t\t|");
+        System.out.println("| Hobi    \t: " + hobi + "\t\t\t|");
+        System.out.println("+-----------------------------------------------+");
+    }
+  }
